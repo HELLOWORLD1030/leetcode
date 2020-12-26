c5f5cee63ac225ee1b58f42260790380b31e4578
HELLO_WORLD <zzg373692503@hotmail.com>
2020-12-26 13:18:08 +0800

    移除元素

--- removeElement.cpp
+++ removeElement.cpp
@@ -1,4 +1,30 @@
 //
 // Created by HELLO WORLD on 2020/12/25.
 //
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int first=nums.size();
+        for(int i=0;i<first;i++){
+            if(nums[i]==val){
+                swap(nums[i],nums[first-1]);
+                i--;
+                first--;
+            }
+        }
+        return first;
+    }
 
+};
+int main(){
+    Solution* solu=new Solution();
+    vector<int> test={0,1,2,2,3,0,4,2};
+    cout<<solu->removeElement(test,2);
+    cout<<endl;
+    for(int i=0;i<test.size();i++){
+        cout<<test[i]<<" ";
+    }
+}
